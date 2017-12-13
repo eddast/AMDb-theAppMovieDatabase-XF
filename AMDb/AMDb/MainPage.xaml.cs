5a23bb219ce588a69ce5fe2e73dcfd7f672a62e6
eddast <eddasr15@ru.is>
2017-12-13 13:16:23 +0000

    Minimalized code in codebehind on search page

--- AMDb/AMDb/MainPage.xaml.cs
+++ AMDb/AMDb/MainPage.xaml.cs
@@ -17,28 +17,8 @@ namespace AMDb
         {
             InitializeComponent();
             this._server = server;
+            BindingContext = new SearchViewModel(Navigation, server);
         }
 
-        private async void MovieButton_Clicked(object sender, EventArgs e)
-        {
-            // Indicate background activity by enabling spinner
-            this.LoadSpinner.IsRunning = true;
-            this.LoadSpinner.IsVisible = true;
-            this.MovieButton.IsEnabled = false;
-            this.MovieButton.BackgroundColor = Color.LightGray;
-
-            if (MovieEntry.Text != null && MovieEntry.Text != "") {
-
-                var movieList = await _server.GetBasicMovieInfoByTitleAsync(this.MovieEntry.Text);
-                await Navigation.PushAsync(new MovieList(movieList, _server));
-            } else { /* TODO */ }
-
-            // Disable spinner and clear text after resolving request
-            this.MovieButton.IsEnabled = true;
-            this.LoadSpinner.IsRunning = false;
-            this.LoadSpinner.IsVisible = false;
-            this.MovieButton.BackgroundColor = Color.DeepSkyBlue;
-            this.MovieEntry.Text = "";
-        }
     }
 }
