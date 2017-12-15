735f546f565ff3cdf4bd0c9592f818cf2e96ad31
eddast <eddasr15@ru.is>
2017-12-15 16:39:33 +0000

    Gledileg jol farin i fri bye

---
+++ AMDb/AMDb/InitializeTabbedPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AMDb
+{
+    public partial class InitializeTabbedPage : TabbedPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await ((TopRatedPage)((NavigationPage)Children[1]).RootPage).viewModel.GetListAsync();
+            await ((PopularMoviesPage)((NavigationPage)Children[2]).RootPage).viewModel.GetListAsync();
+        }
+    }
+}
